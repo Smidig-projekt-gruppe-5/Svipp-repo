118fb75c5b36f6de4b08135d51dda8b5c2db7c82
Kasper Espenes <kaes005@student.kristiania.no>
2025-12-02 09:09:10 +0100

    heihei

---
+++ Svipp/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Svipp
+//
+//  Created by Kasper Espenes on 02/12/2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
