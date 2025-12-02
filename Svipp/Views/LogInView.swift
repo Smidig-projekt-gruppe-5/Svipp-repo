621797d7de3482cdf00b4a6a2b78700cc3e494c3
Kasper Espenes <kaes005@student.kristiania.no>
2025-12-02 09:13:21 +0100

    helenaaaa

---
+++ Svipp/Views/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  Svipp
+//
+//  Created by Kasper Espenes on 02/12/2025.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LogInView()
+}
