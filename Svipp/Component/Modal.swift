04c0220630f8498ffc153c7773b960b54f2e0b60
Hanan Moussa <hmo@zrch.com>
2025-12-02 08:51:12 +0100

    test

--- Svipp/Component/Modal.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Modal.swift
-//  Svipp
-//
-//  Created by lcee heler on 01/12/2025.
-//
-
---
+++ Svipp/Components/Modal.swift
@@ -0,0 +1,18 @@
+//
+//  Modal.swift
+//  Svipp
+//
+//  Created by Hannan Moussa on 02/12/2025.
+//
+
+import SwiftUI
+
+struct Modal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Modal()
+}
